cfffc981d25c8f47179eb8093e468aeda78e9fa5
Anthony Bailey <ambailey1@student.fullsail.edu>
2025-09-21 21:15:44 -0700

    Updated GA_PrimaryAttack to use Child logic from GA_Base Class

--- Source/FantasySurvival/Private/AbilitySystem/Abilities/FS_GA_PrimaryAttack.cpp
+++ Source/FantasySurvival/Private/AbilitySystem/Abilities/FS_GA_PrimaryAttack.cpp
@@ -2,16 +2,12 @@
 
 
 #include "AbilitySystem/Abilities/FS_GA_PrimaryAttack.h"
-#include "AbilitySystem/FS_AbilitySystemComponent.h"
 #include "Player/FS_PlayerState.h"
-#include "Characters/FS_PlayerClass.h" // your enum for classes
-
+#include "Characters/FS_PlayerClass.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemGlobals.h"
-#include "AbilitySystemBlueprintLibrary.h"
-#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
-#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
-
 #include "GameFramework/Character.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Animation/AnimMontage.h"
@@ -21,75 +17,55 @@
 static FGameplayTag TAG_Ability_PrimaryAttack = FGameplayTag::RequestGameplayTag(TEXT("Ability.PrimaryAttack"));
 static FGameplayTag TAG_Event_MeleeHitWindow = FGameplayTag::RequestGameplayTag(TEXT("Event.MeleeHitWindow"));
 static FGameplayTag TAG_Cooldown_PrimaryAttack = FGameplayTag::RequestGameplayTag(TEXT("Cooldown.PrimaryAttack"));
+static FGameplayTag TAG_State_Blocking = FGameplayTag::RequestGameplayTag(TEXT("State.Blocking"));
+static FGameplayTag TAG_State_Attacking = FGameplayTag::RequestGameplayTag(TEXT("State.Attacking"));
 
 UFS_GA_PrimaryAttack::UFS_GA_PrimaryAttack()
 {
-    InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerExecution;
-    NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::ServerInitiated;
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerExecution;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::ServerInitiated;
 
-    FGameplayTagContainer DefaultTags; DefaultTags.AddTag(TAG_Ability_PrimaryAttack);
+    // Asset Tag Metadata
+    FGameplayTagContainer DefaultTags;
+    DefaultTags.AddTag(TAG_Ability_PrimaryAttack);
     SetAssetTags(DefaultTags);
+
+    // Tags applied to the owner while this ability is active
     ActivationOwnedTags.AddTag(TAG_Ability_PrimaryAttack);
+    ActivationOwnedTags.AddTag(TAG_State_Attacking); // Player owns "Attacking" while this runs
+
+    // Tags that prevent this ability from starting
+    ActivationBlockedTags.AddTag(TAG_State_Blocking); // Cant start if currently blocking
 }
 
-UAnimMontage* UFS_GA_PrimaryAttack::ResolveClassMontage(const FGameplayAbilityActorInfo* ActorInfo) const
+void UFS_GA_PrimaryAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
-    if (!ActorInfo) return nullptr;
-    const AFS_PlayerState* PS = Cast<AFS_PlayerState>(ActorInfo->OwnerActor.Get());
-    if (!PS) return nullptr;
+	if (!CommitOrEnd()) return;
 
-    switch (PS->SelectedClass)
-    {
-    case EFSPlayerClass::Warrior:  return WarriorMontage;
-    case EFSPlayerClass::Mage:     return MageMontage;
-    case EFSPlayerClass::Assassin: return AssassinMontage;
-    case EFSPlayerClass::Ranger:   return RangerMontage;
-    default: return WarriorMontage;
-    }
-}
+	// Explicitly apply cooldown GE (in addition to the Ability's internal cooldown system)
+	if (CooldownEffectClass)
+	{
+		FGameplayEffectSpecHandle CD = MakeOutgoingGameplayEffectSpec(CooldownEffectClass, GetAbilityLevel());
+		if (CD.IsValid())
+		{
+			ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, CD);
+		}
+	}
 
-void UFS_GA_PrimaryAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-    const FGameplayAbilityActorInfo* ActorInfo,
-    const FGameplayAbilityActivationInfo ActivationInfo,
-    const FGameplayEventData* TriggerEventData)
-{
-    if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
-    {
-        EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
-        return;
-    }
+	// Wait for the hit-window event (child-specific logic)
+	if (UAbilityTask_WaitGameplayEvent* WaitEvent = WaitForEventTag(TAG_Event_MeleeHitWindow, /*Once*/false, /*Exact*/false))
+	{
+		WaitEvent->EventReceived.AddDynamic(this ,&UFS_GA_PrimaryAttack::OnHitWindowEvent);
+		WaitEvent->ReadyForActivation();
+	}
 
-    bConsumedHitWindow = false;
-
-    ACharacter* Character = Cast<ACharacter>(ActorInfo ? ActorInfo->AvatarActor.Get() : nullptr);
-    UAnimMontage* AttackMontage = ResolveClassMontage(ActorInfo);
-    if (!Character || !AttackMontage)
-    {
-        EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
-        return;
-    }
-
-    if (UAbilityTask_WaitGameplayEvent* WaitEvent =
-        UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, TAG_Event_MeleeHitWindow, nullptr, false, false))
-    {
-        WaitEvent->EventReceived.AddDynamic(this, &UFS_GA_PrimaryAttack::OnHitWindowEvent);
-        WaitEvent->ReadyForActivation();
-    }
-
-    MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
-        this, NAME_None, AttackMontage, 1.f, NAME_None, false);
-
-    if (MontageTask)
-    {
-        MontageTask->OnCompleted.AddDynamic(this, &UFS_GA_PrimaryAttack::OnMontageCompleted);
-        MontageTask->OnInterrupted.AddDynamic(this, &UFS_GA_PrimaryAttack::OnMontageCompleted);
-        MontageTask->OnCancelled.AddDynamic(this, &UFS_GA_PrimaryAttack::OnMontageCompleted);
-        MontageTask->ReadyForActivation();
-    }
-    else
-    {
-        EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
-    }
+	// Ask base to start the montage with default bindings (it will EndAbility on finish)
+	UAnimMontage* AttackMontage = ResolveMontage(ActorInfo);
+	if (!BeginAbilityMontage(AttackMontage, /*Rate*/1.0f, /*Section*/NAME_None, /*bStopWhenAbilityEnds*/false))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, /*bReplicateEndAbility*/true, /*bWasCancelled*/true);
+		return;
+	}
 }
 
 void UFS_GA_PrimaryAttack::OnHitWindowEvent(FGameplayEventData Payload)
@@ -152,15 +128,18 @@ void UFS_GA_PrimaryAttack::OnHitWindowEvent(FGameplayEventData Payload)
     }
 }
 
-void UFS_GA_PrimaryAttack::OnMontageCompleted()
+UAnimMontage* UFS_GA_PrimaryAttack::ResolveMontage(const FGameplayAbilityActorInfo* ActorInfo) const
 {
-    EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false, false);
-}
-
-void UFS_GA_PrimaryAttack::EndAbility(const FGameplayAbilitySpecHandle Handle,
-    const FGameplayAbilityActorInfo* ActorInfo,
-    const FGameplayAbilityActivationInfo ActivationInfo,
-    bool bReplicateEndAbility, bool bWasCancelled)
-{
-    Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+    if (!ActorInfo) return nullptr;
+    const AFS_PlayerState* PS = Cast<AFS_PlayerState>(ActorInfo->OwnerActor.Get());
+    if (!PS) return nullptr;
+    
+    switch (PS->SelectedClass)
+    {
+        case EFSPlayerClass::Warrior:  return WarriorMontage;
+        case EFSPlayerClass::Mage:     return MageMontage;
+        case EFSPlayerClass::Assassin: return AssassinMontage;
+        case EFSPlayerClass::Ranger:   return RangerMontage;
+        default: return WarriorMontage;
+    }
 }
--- Source/FantasySurvival/Public/AbilitySystem/Abilities/FS_GA_PrimaryAttack.h
+++ Source/FantasySurvival/Public/AbilitySystem/Abilities/FS_GA_PrimaryAttack.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
 #include "GameplayTagContainer.h"
+#include "AbilitySystem/Abilities/FS_GA_Base.h"
 #include "FS_GA_PrimaryAttack.generated.h"
 
 class UAnimMontage;
@@ -12,58 +13,43 @@ class UAbilityTask_PlayMontageAndWait;
 
 
 UCLASS()
-class FANTASYSURVIVAL_API UFS_GA_PrimaryAttack : public UGameplayAbility
+class FANTASYSURVIVAL_API UFS_GA_PrimaryAttack : public UFS_GA_Base
 {
 	GENERATED_BODY()
 	
 public:
-    UFS_GA_PrimaryAttack();
+	UFS_GA_PrimaryAttack();
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Effects")
-    TSubclassOf<class UGameplayEffect> CooldownEffectClass;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Effects")
+	TSubclassOf<class UGameplayEffect> CooldownEffectClass;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Effects")
-    TSubclassOf<class UGameplayEffect> DamageEffectClass;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Effects")
+	TSubclassOf<class UGameplayEffect> DamageEffectClass;
 
-    // Per-class montages (assign in asset or via BP child if you prefer)
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Animation")
-    TObjectPtr<UAnimMontage> WarriorMontage;
+	// Per-Class Montages
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Animation") UAnimMontage* WarriorMontage = nullptr;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Animation") UAnimMontage* MageMontage = nullptr;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Animation") UAnimMontage* AssassinMontage = nullptr;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Animation") UAnimMontage* RangerMontage = nullptr;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Animation")
-    TObjectPtr<UAnimMontage> MageMontage;
+	// Hit window settings
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Hit") FName TraceSocketName = TEXT("WeaponTip");
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Hit") float TraceRange = 175.0f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Hit") float TraceRadius = 25.0f;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Animation")
-    TObjectPtr<UAnimMontage> AssassinMontage;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Animation")
-    TObjectPtr<UAnimMontage> RangerMontage;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Hit")
-    FName TraceSocketName = TEXT("WeaponTip");
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Hit")
-    float TraceRange = 175.f;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Hit")
-    float TraceRadius = 25.f;
-
-    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-        const FGameplayAbilityActorInfo* ActorInfo,
-        const FGameplayAbilityActivationInfo ActivationInfo,
-        const FGameplayEventData* TriggerEventData) override;
-
-    virtual void EndAbility(const FGameplayAbilitySpecHandle Handle,
-        const FGameplayAbilityActorInfo* ActorInfo,
-        const FGameplayAbilityActivationInfo ActivationInfo,
-        bool bReplicateEndAbility, bool bWasCancelled) override;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
 
 protected:
-    UFUNCTION() void OnHitWindowEvent(FGameplayEventData Payload);
-    UFUNCTION() void OnMontageCompleted();
+	// Event handlers
+	UFUNCTION() void OnHitWindowEvent(FGameplayEventData Payload);
+
+	// Choose the right montage for the current class
+	virtual UAnimMontage* ResolveMontage(const FGameplayAbilityActorInfo* ActorInfo) const override;
 
 private:
-    UPROPERTY() UAbilityTask_PlayMontageAndWait* MontageTask = nullptr;
-    bool bConsumedHitWindow = false;
+	UPROPERTY() bool bConsumedHitWindow = false;
 
-    UAnimMontage* ResolveClassMontage(const FGameplayAbilityActorInfo* ActorInfo) const;
 };
