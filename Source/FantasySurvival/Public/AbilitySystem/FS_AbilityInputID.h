3cc7b5eb8ec0077d013675a8552a0c3034abdccc
Anthony Bailey <ambailey1@student.fullsail.edu>
2025-09-21 21:14:15 -0700

    Added Block Ability

--- Source/FantasySurvival/Public/AbilitySystem/FS_AbilityInputID.h
+++ Source/FantasySurvival/Public/AbilitySystem/FS_AbilityInputID.h
@@ -10,6 +10,7 @@ enum class EFSAbilityInputID : uint8
 {
 	None			UMETA(DisplayName = "None"),
 	PrimaryAttack	UMETA(DisplayName = "PrimaryAttack"),
+	Block			UMETA(Displayname = "Block"),
 	Ability1		UMETA(DisplayName = "Ability1"),
 	// Add more later (Ability2, Dodge, Ult, etc.)
 };
