558614d89b406e50ba65f770db1a0ba2da358c5a
Anthony Bailey <ambailey1@student.fullsail.edu>
2025-10-06 19:39:18 -0700

    GameMode uses PlayerController. Retargeted animations for ABP, Created IA_PrimaryAttack, Created WBP PlayerHUDWidget

--- Source/FantasySurvival/Private/System/FS_GameMode.cpp
+++ Source/FantasySurvival/Private/System/FS_GameMode.cpp
@@ -4,12 +4,12 @@
 #include "System/FS_GameMode.h"
 #include "Characters/FS_Character.h"
 #include "AbilitySystem/FS_PlayerState.h"
-//#include "System/FS_PlayerController.h"
+#include "System/FS_PlayerController.h"
 
 AFS_GameMode::AFS_GameMode()
 {
 	// Assign defaults (Swap to our custom PC/HUD when ready)
 	DefaultPawnClass = AFS_Character::StaticClass();
 	PlayerStateClass = AFS_PlayerState::StaticClass();
-	//PlayerControllerClass = AFS_PlayerController::StaticClass();
+	PlayerControllerClass = AFS_PlayerController::StaticClass();
 }
